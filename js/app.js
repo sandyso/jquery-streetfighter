90a3e9ea7d02a25e2c21b22edba24aa59d7d2aa0
sandyso <sandyrasom@gmail.com>
2016-03-23 16:16:22 -0400

    everything but sound

--- js/app.js
+++ js/app.js
@@ -3,8 +3,29 @@ $(document).ready(function() {
 	$('.ryu-still').hide();
 	$('.ryu-ready').show();
 })
-	$('.ryu').mouseleave(function() {
+	
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+
+	.mousedown(function() {
+		//play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			);
+		//animate hadouken to the right of screen
+	})
+
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
